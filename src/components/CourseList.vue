607c9e420a8e0d05a05a1ecc8cb71f15a7f83b4d
rafah <rafah.hawa@yahoo.fr>
2024-01-28 12:58:21 +0100

    show courses from temp-data.js

--- src/components/CourseList.vue
+++ src/components/CourseList.vue
@@ -1,7 +1,20 @@
 <template> 
-    <h2>Courses Componenet</h2>
+    <div class="row my-5">
+        <div class="col-lg-3" v-for="course in courses">
+            <div class="card">
+                <img :src="course.image" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ course.name }}</h5>
+                    <p class="card-text"> {{ course.category }}</p>
+                    <p class="card-text"> {{ course.price }}$</p>
+                    <RouterLink :to="'/' + course.id " class="btn btn-primary">Course Detail</RouterLink>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
+    import { courses } from '../temp-data'
     export default {
         name: 'CourseList'
     }
---
+++ src/temp-data.js
@@ -0,0 +1,35 @@
+import img1 from './assets/1.png'
+import img2 from './assets/2.png'
+import img3 from './assets/3.jpg'
+import img4 from './assets/4.webp'
+
+export const courses = [
+    {
+        id:1 , 
+        name: 'Python Basics' , 
+        category: 'Web Development',
+        price: 30 , 
+        image: img1
+    },
+    {
+        id:2 , 
+        name: 'AI Basics' , 
+        category: 'AI Development',
+        price: 40 , 
+        image: img2
+    },
+    {
+        id:3 , 
+        name: 'Flutter Basics' , 
+        category: 'Mobile Development',
+        price: 50 , 
+        image: img3
+    },
+    {
+        id:4 , 
+        name: 'Django Basics' , 
+        category: 'Web Development',
+        price: 60 , 
+        image: img4
+    },
+]
