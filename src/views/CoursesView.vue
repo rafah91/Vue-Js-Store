d0db2a860e91b7292b90c35041a4abe10c064a14
rafah <rafah.hawa@yahoo.fr>
2024-02-04 21:34:51 +0100

    add courses component as props

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -1,20 +1,29 @@
-<template> 
-    
+<template>
+
     <div class="container my-5 ">
         <div class="row">
             <div class="col-lg-3"> <CourseFilter /> </div>
-            <div class="col-lg-9"> <CourseList /> </div>
+            <div class="col-lg-9"> <CourseList :courses="courses" /> </div>
         </div>
     </div>
 </template>
+
+
 <script>
     import CourseList from '@/components/CourseList.vue';
     import CourseFilter from '@/components/CourseFilter.vue';
+    import { courses } from '../temp-data'
+
     export default {
         name: 'CoursesList',
         components:{
             CourseList,
             CourseFilter
+        },
+        data(){
+            return {
+                courses:courses
+            }
         }
     }
 </script>
